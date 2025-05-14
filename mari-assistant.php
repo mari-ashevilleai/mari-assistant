face0989021ea0f01e50764445b1b4f292b1171e
mari-ashevilleai <mari@ashevilleaisolutions.com>
2025-05-14 02:49:21 -0400

    Update mari-assistant.php

--- mari-assistant.php
+++ mari-assistant.php
@@ -12,3 +12,10 @@
  */
 
 defined('ABSPATH') or die('No script kiddies please!');
+function mari_enqueue_scripts() {
+    wp_enqueue_script('mari-popup', plugin_dir_url(__FILE__) . 'js/mari-popup.js', array('jquery'), '1.2.6', true);
+    wp_enqueue_style('mari-style', plugin_dir_url(__FILE__) . 'css/mari-style.css');
+    wp_localize_script('mari-popup', 'mari_ajax_obj', array('ajax_url' => admin_url('admin-ajax.php')));
+}
+add_action('wp_enqueue_scripts', 'mari_enqueue_scripts');
+
